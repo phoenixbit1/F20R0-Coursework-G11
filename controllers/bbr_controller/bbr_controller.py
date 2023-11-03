30abc5cdf5a3a120428a7be7a623d6b5754d0f97
Jacob Turner <jacobmturner@gmail.com>
2023-11-03 11:07:13 +0000

    add run_robot from lab

--- controllers/bbr_controller/bbr_controller.py
+++ controllers/bbr_controller/bbr_controller.py
@@ -42,13 +42,77 @@ class Controller:
         self.inputsPrevious = []
         
         # Flag for if the light was on or off
-        self.flag_light = 0
+        # initially False
+        self.flag_light = False
     # end contructor
     
-    def run_robot():
+    # taken from lab <n>
+    def clip_value(self, value, min_max):
+        if (value > min_max):
+            return min_max
+        elif (value < -min_max):
+            return -min_max
+        return value
+    # end clip_value
+    def sense_compute_actuate(self):
         pass
+    # end sense_compute_actuate
+    
+    # taken from lab <n>
+    def run_robot(self):
+        # main loop
+        count = 0
+        inputs_avg = []
+        while self.robot.step(self.time_step) != -1:
+            # read ground sensors
+            self.inputs = []
+            left = self.left_ir.getValue()
+            center = self.center_ir.getValue()
+            right = self.right_ir.getValue()
+            
+            # change values
+            min_gs = 0
+            max_gs = 1000
+            if(left > max_gs): left = max_gs
+            if(center > max_gs): center = max_gs
+            if(right > max_gs): right = max_gs
+            if(left < min_gs): left = min_gs
+            if(center < min_gs): center = min_gs
+            if(right < min_gs): right = min_gs
+            
+            # save data
+            self.inputs.append((left-min_gs)/(max_gs-min_gs))
+            self.inputs.append((center-min_gs)/(max_gs-min_gs))
+            self.inputs.append((right-min_gs)/(max_gs-min_gs))
+            
+            # read proximity sensors
+            for i in range(8):
+                tmp = self.proximity_sensors[i].getValue()
+                # change values
+                min_ps = 0
+                max_ps = 2400
+                if(tmp > max_ps): tmp = max_ps
+                if(tmp < min_ps): tmp = min_ps
+                # save data
+                self.inputs.append((tmp-min_ps)/(max_ps-min_ps))
+            # end for
+            # smooth filter (average)
+            smooth = 30
+            if(count == smooth):
+                inputs_avg = [sum(x) for x in zip(*inputs_avg)]
+                self.inputs = [x/smooth for x in inputs_avg]
+                # Compute and actuate
+                self.sense_compute_and_actuate()
+                # Reset
+                count = 0
+                inputs_avg = []
+                self.inputsPrevious = self.inputs
+            else:
+                inputs_avg.append(self.inputs)
+                count = count + 1
+        # end while
     # end run_robot
-# enc class Controller
+# end class Controller
 
 if __name__ == "__main__":
     behavioural = Robot()
